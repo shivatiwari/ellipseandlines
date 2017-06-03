fdc9a78686a6dc139cc918753c2bc042b9776de0
shivatiwari <shivansh22t@gmail.com>
2017-06-03 13:01:40 +0530

    Add files via upload

---
+++ view.js
@@ -0,0 +1,21 @@
+//creating Objects with physics
+function setup()
+{
+    createCanvas(outerWidth,outerHeight);
+    background(random(255));
+}
+function draw()
+{
+     //background(255);
+   //if(mouseIsPressed)
+    //    {
+    fill(255);
+    strokeWeight(5);
+    stroke(random(255,0,0));
+    line(mouseX,mouseY,random(0,1000),random(0,1000));
+    //stroke(255,0,0);
+    line(mouseX,mouseY,random(0,width),random(0,height));
+    
+    ellipse(mouseX,mouseY,40,40);
+//}
+}
